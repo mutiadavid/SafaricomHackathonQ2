9c587593311d2b15fe69b51ac056a963a5cefe75
david mutia <davidm@pixsel.net>
2019-06-12 22:28:37 +0300

    Add DeletedColumn Migration.

---
+++ SafaricomHackathonQ2.Data/Migrations/20190612192559_DeletedColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SafaricomHackathonQ2.Data.Migrations
+{
+    public partial class DeletedColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "CreditVouchers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "CreditVouchers");
+        }
+    }
+}
